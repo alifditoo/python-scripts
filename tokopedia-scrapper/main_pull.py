145a3bf114df7ac9c1ae7f4bc1052c178b1f12a9
alifditoo <alifditoo@gmail.com>
2025-02-12 10:50:26 +0700

    Update main_pull.py

--- tokopedia-scrapper/main_pull.py
+++ tokopedia-scrapper/main_pull.py
@@ -10,6 +10,8 @@ from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchWindowException, InvalidSessionIdException, WebDriverException
+from requests.exceptions import ReadTimeout as ReadTimeoutError
 
 #-- Headless Mode --#
 browser_options = Options()
